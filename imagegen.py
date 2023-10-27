d2758efb4d62ce21cf34056d7d634fb81e030782
Chen Jiasen <jiasennn@gmail.com>
2023-10-27 16:39:19 +0800

    imagengen fit

--- imagegen.py
+++ imagegen.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import customtkinter as ctk 
 
 from PIL import ImageTk
-from apikey import api_token
+from apikey import apikey as auth_token
 
 import torch
 from torch import autocast
@@ -14,10 +14,10 @@ app.geometry("532x632")
 app.title("Stable Bud") 
 ctk.set_appearance_mode("dark") 
 
-prompt = ctk.CTkEntry(height=40, width=512, text_font=("Arial", 20), text_color="black", fg_color="white") 
+prompt = ctk.CTkEntry(app, height=40, width=512, font=("Arial", 20), text_color="black", fg_color="white") 
 prompt.place(x=10, y=10)
 
-lmain = ctk.CTkLabel(height=512, width=512)
+lmain = ctk.CTkLabel(app, height=512, width=512)
 lmain.place(x=10, y=110)
 
 modelid = "CompVis/stable-diffusion-v1-4"
